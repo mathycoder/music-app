f947abaa5db41a301435cc43924acda8649f7e62
Adam Sarli <adam.sarli@gmail.com>
2020-04-27 12:50:12 -0400

    added basic layout and styling for a demo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import DemoContainer from './containers/DemoContainer'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <body style={styles.containerDiv}>
+        <DemoContainer />
+        <div style={styles.bottomDiv}>hi</div>
+      </body>
     </div>
   );
+}
+
+const styles = {
+  containerDiv: {
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100vh'
+  },
+  bottomDiv: {
+    flex: 1,
+    backgroundColor: 'rgb(95,95,95)'
+  }
 }
 
 export default App;
---
+++ src/containers/DemoContainer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './css/demo-styles.css'
+
+const DemoContainer = () => {
+  return (
+    <div className="demo-wrapper">
+      <div className="note">C</div>
+      <div className="note">D</div>
+      <div className="note">E</div>
+    </div>
+  )
+}
+
+export default DemoContainer
